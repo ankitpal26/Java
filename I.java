65ecca8fd2805747bfc2d4cc1c174ce62c9df49b
ankitpal26 <76256647+ankitpal26@users.noreply.github.com>
2022-02-06 11:57:58 +0530

    Add files via upload

---
+++ I.java
@@ -0,0 +1,24 @@
+public class I{
+    static int test1()
+    {
+        System.out.println("from test1");
+        return 10;
+    }
+    static int test2(){
+        System.out.println("from test2");
+        return test1();
+    }
+    static int test3(){   
+        System.out.println("from test 3");
+        return test1()+test2();
+
+    }
+    public static void main(String []args)
+    {
+        
+        System.out.println(test1());
+        System.out.println(test2());
+        System.out.println(test3());
+
+    }
+}
